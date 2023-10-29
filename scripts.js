d155c0b32e4adb30624c0964d16ddacab71b7aad
Joey <info@techbyjoe.co>
2023-10-28 22:02:08 -0400

    More major changes - code nearly final

--- scripts.js
+++ scripts.js
@@ -15,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let navPosMobile = navbarMobile.getBoundingClientRect().top;
   let spacerMobile = document.createElement('div'); // Create a new spacer element
   spacerMobile.style.height = `${navbarMobile.offsetHeight}px`; // Set its height
+  spacerMobile.style.transition = "transform 0.3s ease"; // Add a CSS transition for smoothness using the transform property
 
   if (window.screen.width <= 600) {
     let isMobileSticky = false;
@@ -25,7 +26,9 @@ document.addEventListener("DOMContentLoaded", function () {
         if (!isMobileSticky) {
           setTimeout(() => {
             navbarMobile.classList.add('sticky');
-            navbarMobile.parentNode.insertBefore(spacerMobile, navbarMobile); // Insert the spacer
+            // Insert the spacer
+            spacerMobile.textContent = '';
+            navbarMobile.parentNode.insertBefore(spacerMobile, navbarMobile);
           }, 100); // Adjust the delay as needed
           isMobileSticky = true;
         }
@@ -33,7 +36,9 @@ document.addEventListener("DOMContentLoaded", function () {
         if (isMobileSticky) {
           setTimeout(() => {
             navbarMobile.classList.remove('sticky');
-            spacerMobile.remove(); // Remove the spacer
+            if (spacerMobile.parentNode === navbarMobile.parentNode) {
+              spacerMobile.remove(); // Remove the spacer
+            }
           }, 100); // Adjust the delay as needed
           isMobileSticky = false;
         }
@@ -44,10 +49,9 @@ document.addEventListener("DOMContentLoaded", function () {
   // Desktop Navigation Sticky
   let navbar = document.getElementById("navHeader");
   let topNav = document.getElementById("topNav");
-  let spacer = document.createElement('div'); // Create a new spacer element
-  spacer.style.transition = "all 1.75s ease-in-out"
-
-  let textSpacer = null; // Define textSpacer outside the if block
+  let spacer = document.createElement('div'); // Create a single spacer element
+  spacer.style.transition = "transform 0.3s ease"; // Add a CSS transition for smoothness using the transform property
+  spacer.style.height = "4vw";
 
   if (window.screen.width > 600) {
     let isDesktopSticky = false;
@@ -55,18 +59,18 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", e => {
       let navPos = navbar.getBoundingClientRect().top;
       let scrollPos = window.scrollY;
-      var spacerValue = 64; // Change the variable name to avoid confusion
+      var spacerValue = 115;
       if (window.screen.width > 600 && window.screen.width < 700) {
-        spacerValue = 120;
+        spacerValue = 220;
       }
       if (scrollPos > navPos + spacerValue) {
         if (!isDesktopSticky) {
           setTimeout(() => {
             navbar.classList.add('sticky');
             topNav.classList.add('sticky');
-            // Create a text node to use as a spacer
-            textSpacer = document.createTextNode(' ');
-            navbar.parentNode.insertBefore(textSpacer, navbar); // Insert the spacer
+            // Insert the spacer
+            spacer.textContent = '';
+            navbar.parentNode.insertBefore(spacer, navbar);
           }, 100); // Adjust the delay as needed
           isDesktopSticky = true;
         }
@@ -75,9 +79,8 @@ document.addEventListener("DOMContentLoaded", function () {
           setTimeout(() => {
             navbar.classList.remove('sticky');
             topNav.classList.remove('sticky');
-            if (textSpacer) {
-              textSpacer.remove(); // Remove the spacer
-              textSpacer = null; // Reset textSpacer
+            if (spacer.parentNode === navbar.parentNode) {
+              spacer.remove(); // Remove the spacer
             }
           }, 100); // Adjust the delay as needed
           isDesktopSticky = false;
@@ -89,8 +92,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
-
+//Custom Scroll to Target
 function scrollToTarget(targetId) {
   var offset = -40;
   if (window.screen.width <= 600) {
@@ -106,14 +108,16 @@ function scrollToTarget(targetId) {
       behavior: 'smooth' // 'auto' or 'smooth' for scrolling behavior
     });
     if (window.screen.width <= 600) {
-      console.log("this should run closeNav()...")      
-      closeNav();
+      console.log("this should run closeNav()...")
+      document.getElementById("mobileNavOverlay").style.height = "0%";
     }
   }
 }
 
+// Fullscreen 
 function openFullscreen(content) {
-  if (window.screen.width < 600) {
+  // if (window.screen.width < 600) {
+  if (window.screen.width > 1) {
     open(content, '_blank');
   } else {
     var fullscreenContent = document.getElementById("fullscreen");
@@ -146,22 +150,26 @@ function openFullscreen(content) {
 }
 
 
-function closeFullscreen() {
-  fullscreenContent.innerHTML = '<div id="exit" onclick="closeFullscreen()">Exit</div>'
-  fullscreenContent.style.display = "none";
-  document.getElementById("topNav").style.display = "block";
-  document.body.classList.remove("no-scroll");
-}
 
-function hideSpinner() {
-  document.querySelector('.spinner').style.display = 'none';
-}
 
-document.addEventListener('keydown', evt => {
-  if (evt.key === 'Escape') {
-    closeFullscreen();
-  }
-});
+// function closeFullscreen() {
+//   fullscreenContent.innerHTML = '<div id="exit" onclick="closeFullscreen()">Exit</div>'
+//   fullscreenContent.style.display = "none";
+//   document.getElementById("topNav").style.display = "block";
+//   document.body.classList.remove("no-scroll");
+// }
+
+// function hideSpinner() {
+//   document.querySelector('.spinner').style.display = 'none';
+// }
+
+// document.addEventListener('keydown', evt => {
+//   if (evt.key === 'Escape') {
+//     closeFullscreen();
+//   }
+// });
+
+
 
 // const targets = document.getElementsByClassName('shiny');
 
@@ -176,11 +184,13 @@ document.addEventListener('keydown', evt => {
 
 //   const target = event.currentTarget;
 
-//   const yAxisDegree = event.pageX / width * 45 - 5;
-//   const xAxisDegree = event.pageY / height * -1 * 45 + 5;
+//   // Reduce the rotation angles for a less pronounced effect
+//   // Adjust these values to control the intensity of the perspective effect
+//   const yAxisDegree = event.pageX / width * 3 - 1.5; // Adjust the multiplication factor (e.g., 3) and constant values (e.g., -1.5)
+//   const xAxisDegree = event.pageY / height * -3 + 1.5; // Adjust the multiplication factor (e.g., 3) and constant values (e.g., 1.5)
 //   target.style.transform = `rotateY(${yAxisDegree}deg) rotateX(${xAxisDegree}deg)`;
 
-//   setSheenPosition(target, event.pageX / width, event.pageY / width);
+//   setSheenPosition(target, event.pageX / width, event.pageY / height);
 // }
 
 // function handleMouseLeave(event) {
@@ -202,8 +212,9 @@ document.addEventListener('keydown', evt => {
 // }
 
 // function setSheenPosition(target, xRatio, yRatio) {
-//   const xOffset = 1 - (xRatio - 0.5) * 800;
-//   const yOffset = 1 - (yRatio - 0.5) * 800;
+//   // Adjust these values to control the sheen position effect
+//   const xOffset = 1 - (xRatio - 0.5) * 0.1; // Adjust the multiplication factor (e.g., 0.1)
+//   const yOffset = 1 - (yRatio - 0.5) * 0.1; // Adjust the multiplication factor (e.g., 0.1)
 //   target.style.setProperty('--sheenX', `${xOffset}px`);
 //   target.style.setProperty('--sheenY', `${yOffset}px`);
 // }
