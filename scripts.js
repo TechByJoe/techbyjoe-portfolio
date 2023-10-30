bbfcfc621b3a58cc70a99494fb4752337215a730
Joey <info@techbyjoe.co>
2023-10-30 11:56:12 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -135,7 +135,7 @@ function updateThemeColor() {
     document.documentElement.offsetHeight
   );
 
-  const threshold = 0.80;
+  const threshold = 0.71;
 
   if (scrollPosition >= threshold * documentHeight) {
     themeColorMetaTag.setAttribute("content", "#262626"); // Change to black
