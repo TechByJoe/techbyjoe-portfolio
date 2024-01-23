959994bdf0fa746df54cb1e4bdf5e6767aa03c6b
Joey <info@techbyjoe.co>
2024-01-22 22:30:13 -0500

    Major overhaul, links to typedream

--- scripts.js
+++ scripts.js
@@ -63,48 +63,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Desktop Navigation Sticky
-  if (window.screen.width > 600) {
-    let navbar = document.getElementById("navHeader");
-    let topNav = document.getElementById("topNav");
-    let spacer = document.createElement('div'); // Create a single spacer element
-    spacer.style.transition = "transform 0.3s ease"; // Add a CSS transition for smoothness using the transform property
-    spacer.style.height = "4vw";
-    let isDesktopSticky = false;
-
-    window.addEventListener("scroll", e => {
-      let navPos = navbar.getBoundingClientRect().top;
-      let scrollPos = window.scrollY;
-      var spacerValue = 115;
-      if (window.screen.width > 600 && window.screen.width < 700) {
-        spacerValue = 220;
-      }
-      if (scrollPos > navPos + spacerValue) {
-        if (!isDesktopSticky) {
-          setTimeout(() => {
-            navbar.classList.add('sticky');
-            topNav.classList.add('sticky');
-            // Insert the spacer
-            spacer.textContent = '';
-            navbar.parentNode.insertBefore(spacer, navbar);
-          }, 100); // Adjust the delay as needed
-          isDesktopSticky = true;
-        }
-      } else {
-        if (isDesktopSticky) {
-          setTimeout(() => {
-            navbar.classList.remove('sticky');
-            topNav.classList.remove('sticky');
-            if (spacer.parentNode === navbar.parentNode) {
-              spacer.remove(); // Remove the spacer
-            }
-          }, 100); // Adjust the delay as needed
-          isDesktopSticky = false;
-        }
-      }
-    });
-  }
-
 
   //Change theme color on scroll
   if (window.screen.width < 600) {
@@ -134,8 +92,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-
-//Custom Scroll to Target
+// Custom Scroll to Target
 function scrollToTarget(targetId) {
   var offset = -40;
   if (window.screen.width <= 600) {
